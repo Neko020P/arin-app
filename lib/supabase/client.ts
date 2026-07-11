046ee6e298f45528e0262bd0956e12666873cc15
Chomphunuch <chomphunuch1346@gmail.com>
2026-07-11 16:44:42 +0700

    dev14

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,16 @@
+// arin/lib/supabase/client.ts
 import { createBrowserClient } from '@supabase/ssr'
+import type { SupabaseClient } from '@supabase/supabase-js'
+
+let client: SupabaseClient | undefined
 
 export function createClient() {
-  return createBrowserClient(
+  if (client) return client
+
+  client = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
+
+  return client
 }
